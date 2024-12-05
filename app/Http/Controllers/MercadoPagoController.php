af42fc7d3b25d3d5adab2048851917494b9943c9
SamuelCoder1 <echeverrysamuel74@gmail.com>
2024-12-05 02:07:37 -0500

    fix: repair error in the mercado pago config

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Services\MercadoPagoService;
 use Illuminate\Http\Request;
+use App\Events\ErrorOccurred;
+use Illuminate\Support\Facades\Log;
 
 class MercadoPagoController extends Controller
 {
@@ -26,26 +28,31 @@ class MercadoPagoController extends Controller
         $items = [
             [
                 "id" => "1234567890",
-                "title" => "Producto 1",
-                "description" => "Descripción del producto 1",
-                "currency_id" => "BRL",
+                "title" => "Producto 1", 
+                "currency_id" => "COP",  // Moneda de Colombia
                 "quantity" => 1,
-                "unit_price" => 100.00
+                "unit_price" => 100.00  // Precio en COP
             ]
         ];
 
         $payer = [
-            "name" => $request->input('name'),
-            "surname" => $request->input('surname'),
-            "email" => $request->input('email'),
+            "name" => "TESTUSER800659108",  // Nombre del comprador
+            "surname" => "TESTUSER",        // Apellido del comprador
+            "email" => "testuser800659108@test.com", // Correo electrónico ficticio
+            "identification" => [
+                "type" => "CC",  // Tipo de documento para Colombia es CC (Cédula de Ciudadanía)
+                "number" => "123456789"  // Número de cédula ficticio
+            ]
         ];
 
         $preference = $this->mercadoPagoService->createPaymentPreference($items, $payer);
 
         if ($preference) {
-            return redirect($preference->init_point); // Redirige a MercadoPago para el pago
+            return redirect($preference->init_point); // Redirige al sandbox de MercadoPago.
         } else {
-            return redirect()->route('mercadopago.failed');
+            Log::error('Mercado Pago error:', ['message' => 'No se pudo crear la preferencia de pago.']);
+            event(new ErrorOccurred('Error al pagar con Mercado Pago', 'No se pudo crear la preferencia.'));
+            return redirect()->route('mercadopago.payment')->with('error', 'Error al crear la preferencia de pago.');
         }
     }
 
